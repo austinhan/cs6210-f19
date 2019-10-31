2bae90fdf2fcb8178ed339055754105fded0073c
Austin Han <ah2262@cornell.edu>
2019-10-31 11:33:51 -0400

    Completed w/ FD derivative, but LU not used yet

--- hw/code/hw7newton.m
+++ hw/code/hw7newton.m
@@ -15,5 +15,17 @@ function [sigma, gs] = hw7newton(A, b, c, sigma)
     %   Compute g(sigma) and save to gs(k), then 
     %   compute a Newton step to update the
     %   eigenvalue estimate sigma.
+    x = M\en;
+    gs(k)=x(end);
+%     ddet = trace(adjoint(A-sigma*I));
+%     dg = ddet/det(-b*c');
+%     sigma=sigma-gs(k)/dg;
+    M = [A-(sigma+1e-6)*I, b; c', 0];
+    x2 = M\en;
+    M = [A-(sigma-1e-6)*I, b; c', 0];
+    x1 = M\en;
+    dgs = (x2(end)-x1(end))/2e-6;
+    sigma = sigma - gs(k)/dgs;
+
 
   end
