35b94bb419ca79dd717d5d0e7d75c1a8872ce0bb
Austin Han <ah2262@cornell.edu>
2019-11-16 02:56:25 -0500

    Finished hw8 code

--- hw/code/hw8solve.m
+++ hw/code/hw8solve.m
@@ -4,27 +4,29 @@
 %   minimize x'Ax/2 - x'b s.t. x'x = 1
 % Your code should run in an overall time of O(n^3).
 %
-function [x,phi] = hw8solve(A, b)
+function [x,mu_x,phi] = hw8solve(A, b)
 
 n = length(b);
 % Find all possible multipliers (O(n^3) time)
 mu = polyeig(A^2-b*b',-2*A,eye(n));
 mu = sort(real(mu(imag(mu)==0)));
 
-% x = (A-mu(1)*eye(n))\b;
-% phi = 1/2*x'*A*x-x'*b;
-% 
-% for i = 2:length(mu)
-%     x_test = (A-mu(i)*eye(n))\b;
-%     phi_test = 1/2*x_test'*A*x_test-x_test'*b;
-%     if phi_test < phi
-%         phi = phi_test;
-%         x = x_test;
-%     end
-% end
+x = (A-mu(1)*eye(n))\b;
+phi = 1/2*x'*A*x-x'*b;
+mu_x = mu(1);
 
-for i = 1:length(mu)
-    x(:,i) = (A-mu(i)*eye(n))\b;
-    phi(i) = 1/2*x(:,i)'*A*x(:,i)-x(:,i)'*b;
+for i = 2:length(mu)
+    x_test = (A-mu(i)*eye(n))\b;
+    phi_test = 1/2*x_test'*A*x_test-x_test'*b;
+    if phi_test < phi
+        phi = phi_test;
+        x = x_test;
+        mu_x = mu(i);
+    end
 end
-i=1;
+
+% for i = 1:length(mu)
+%     x(:,i) = (A-mu(i)*eye(n))\b;
+%     phi(i) = 1/2*x(:,i)'*A*x(:,i)-x(:,i)'*b;
+% end
+% i=1;
--- hw/code/hw8test.m
+++ hw/code/hw8test.m
@@ -2,3 +2,31 @@
 %  a.  The multipliers correspond to stationary points
 %  b.  The solution returned by qcqp_solve is a constrained minimizer
 %  c.  For some reference problem, qcqp_solve returns the global min
+
+n = 10;
+
+% A = eye(n);
+% b = zeros(n,1);
+% [x,phi] = hw8solve(A,b);
+
+A = 2*rand(n) - 1;
+A = triu(A);
+A = A + A' - diag(diag(A));
+b = 2*rand(n,1)-1;
+[x,mu,phi] = hw8solve(A,b);
+
+% a.
+x_test = (A-mu*eye(n))\b;
+
+% b.
+eigvals = eig(A);
+fprintf('mu < lambda_2(A): %i\n',mu<eigvals(2));
+% c.
+% phi = x^2+1/3*x*y+10*y^2-2*x
+% minimum from Wolfram Alpha
+Ac = 2*[1 1/6;1/6 10];
+bc = [2;0];
+[xc,muc,phic] = hw8solve(Ac,bc);
+fprintf('reference min: %d\n',-1.00278);
+fprintf('calculated min: %d\n',phic);
+
